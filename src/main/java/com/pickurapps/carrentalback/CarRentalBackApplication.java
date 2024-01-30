9f61ce806753d3e5f9d14e3c6ce9025fbed5d7e9
Ala Ben Yahia <alabinyahia@gmail.com>
2024-01-30 14:54:46 +0100

    Setup project structure and application config file

---
+++ src/main/java/com/pickurapps/carrentalback/CarRentalBackApplication.java
@@ -0,0 +1,13 @@
+package com.pickurapps.carrentalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalBackApplication.class, args);
+    }
+
+}
